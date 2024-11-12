59ba5f0ce2cdc2c665e627d2cf21f718119a3dff
Dotoritos <wwponv158@gmail.com>
2024-11-12 17:04:01 +0900

    feat: 전역 상태 관리 기능 예시

---
+++ src/Stores/types.ts
@@ -0,0 +1,8 @@
+/**
+ * @template S zustand store 상태
+ * @template K selector가 없는 경우 default로 사용할 key
+ */
+export type SelectorHook<S, K extends keyof S> = {
+    <U>(selector: (state: S) => U): U;
+    (): S[K];
+};
